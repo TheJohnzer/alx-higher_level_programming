9013dec1cbecefc1f10c7c97948315261a667d67
TheJohnzer <axeljonathan208@gmail.com>
2022-08-29 04:56:33 -0700

    fireboy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    length = len(my_list)
+    "replaces an element in a list at idx without modifying the original list"
 
-    new_list = my_list[:]
-
-    if 0 <= idx < length:
-        new_list[idx] = element
-
-        return (new_list)
+    my_list_copy = my_list[:]
+    "if idx is negative or out of range, return a copy of the original list"
+    if idx < 0 or idx >= len(my_list):
+        return (my_list_copy)
+    my_list_copy[idx] = element
+    return (my_list_copy)
